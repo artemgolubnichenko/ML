8ca6b46bbcc9e23ee9ce41b6e80a1ae2136979aa
Artem Golubnichenko <artem.golubnichenko@upandrunningsoftware.com>
2018-12-13 14:42:52 +0600

    Small refactoring

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ import utils
 
 TRACE = 1
 
-
 pd.set_option('display.expand_frame_repr', False)
 
 
-def trace_enabled():
-    return TRACE == 1
+def log(*args):
+    if TRACE == 1:
+        for arg in args:
+            print(arg)
 
 
 path_to_train = os.path.join(configuration.BASE_PATH, 'train')
@@ -26,91 +27,38 @@ path_to_train_csv = os.path.join(configuration.BASE_PATH, 'train.csv')
 path_to_test_csv = os.path.join(configuration.BASE_PATH, 'sample_submission.csv')
 
 labels_data = pd.read_csv(path_to_train_csv)
-if trace_enabled():
-    print(labels_data.shape[0], ' items loaded')
-    print(labels_data.head())
+log(labels_data.shape[0], labels_data.head())
 
 train, test = utils.split_data(labels_data)
-if trace_enabled():
-    print(train.shape[0], ' items in train')
-    print(train.head())
-    print(test.shape[0], ' items in test')
-    print(test.head())
-
-
-def fill_targets(row):
-    row.Target = np.array(row.Target.split(" ")).astype(np.int)
-    for num in row.Target:
-        name = labels.label_names[int(num)]
-        row.loc[name] = 1
-    return row
-
+log(train.shape[0], train.head(), test.shape[0], test.head())
 
 for key in labels.label_names.keys():
     train[labels.label_names[key]] = 0
     test[labels.label_names[key]] = 0
 
+train = train.apply(utils.fill_targets, axis=1)
+test = test.apply(utils.fill_targets, axis=1)
+log(train.head(), test.head())
 
-train = train.apply(fill_targets, axis=1)
-test = test.apply(fill_targets, axis=1)
-if trace_enabled():
-    print(train.head())
-    print(test.head())
-
-
-X_train = train.iloc[:,0]
+X_train = train.iloc[:, 0]
 y_train = train.iloc[:, 2:30]
-X_test = test.iloc[:,0]
+X_test = test.iloc[:, 0]
 y_test = test.iloc[:, 2:30]
-if trace_enabled():
-    print(X_train.head())
-    print(y_train.head())
+log(X_train.head(), y_train.head())
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def ohe(x, n):
-    return np.eye(n)[x]
-
-
-def _parse_function(filename, labels):
-    image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_png(image_string)
-    image_float = tf.image.convert_image_dtype(image_decoded, tf.float32)
-    return tf.reshape(image_float, [512, 512, 1]), labels
-
-
-def wrap_to_input_fn(files, labels, train=True):
-    dataset = tf.data.Dataset.from_tensor_slices((files, labels))
-    dataset = dataset.map(_parse_function)
-    dataset = dataset.batch(50)
-    if train:
-        dataset = dataset.repeat().shuffle(100)
-    return dataset.make_one_shot_iterator().get_next()
-
-# tf_record_pattern = os.path.join(path_to_test, '*_blue.png')
-# X_test = np.array(tf.gfile.Glob(tf_record_pattern))
-# X_test.sort()
-
-# labels.py = np.loadtxt(os.path.join(path_to_data, 'sample_submission.csv'), np.float32, skiprows=1)
-# print(labels.py)
-# y_test = np.array([ohe(int(x), 27) for x in labels.py], np.float32)
-# print(y_test)
-
-# print(labels.label_names)
+# def load_image(self, path):
+#     R = Image.open(path + '_red.png')
+#     G = Image.open(path + '_green.png')
+#     B = Image.open(path + '_blue.png')
+#     Y = Image.open(path + '_yellow.png')
+#
+#     im = np.stack((
+#         np.array(R),
+#         np.array(G),
+#         np.array(B),
+#         np.array(Y)), -1)
+#
+#     im = cv2.resize(im, (SHAPE[0], SHAPE[1]))
+#     im = np.divide(im, 255)
+#     return im
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+import labels
 
 
 def split_data(labels_data):
@@ -8,3 +12,47 @@ def split_data(labels_data):
     train = train.reset_index(drop=True)
     test = test.reset_index(drop=True)
     return train, test
+
+
+def fill_targets(row):
+    row.Target = np.array(row.Target.split(" ")).astype(np.int)
+    for num in row.Target:
+        name = labels.label_names[int(num)]
+        row.loc[name] = 1
+    return row
+
+
+def make_rgb_image_from_four_channels(path, image_width=512, image_height=512) -> np.ndarray:
+    """
+    It makes literally RGB image from source four channels,
+    where yellow image will be yellow color, red will be red and so on
+    """
+    rgb_image = np.zeros(shape=(image_height, image_width, 3), dtype=np.float)
+    yellow = np.array(Image.open(path + '_yellow.png'))
+    # yellow is red + green
+    rgb_image[:, :, 0] += yellow / 2
+    rgb_image[:, :, 1] += yellow / 2
+    # handling for R,G and B channels
+    r_image = Image.open(path + '_red.png')
+    rgb_image[:, :, 0] += r_image
+    g_image = Image.open(path + '_green.png')
+    rgb_image[:, :, 1] += g_image
+    b_image = Image.open(path + '_blue.png')
+    rgb_image[:, :, 2] += b_image
+    # Normalize image
+    rgb_image = rgb_image / rgb_image.max() * 255
+    return rgb_image.astype(np.uint8)
+
+
+def _parse_function(filename, _labels):
+    image_uint8 = make_rgb_image_from_four_channels(filename)
+    return image_uint8, _labels
+
+
+def wrap_to_input_fn(files, _labels, train=True):
+    dataset = tf.data.Dataset.from_tensor_slices((files, _labels))
+    dataset = dataset.map(_parse_function)
+    dataset = dataset.batch(50)
+    if train:
+        dataset = dataset.repeat().shuffle(100)
+    return dataset.make_one_shot_iterator().get_next()
